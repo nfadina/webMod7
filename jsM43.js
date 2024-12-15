03c8bc90d5a6e6a6c9b2a3247c06333f60cddd56
nfadina <nurfaridaaudina@gmail.com>
2024-12-15 20:01:31 +0700

    Add files via upload

---
+++ jsM43.js
@@ -0,0 +1,12 @@
+function pesan() {
+    alert("Ini Latihan Javascript Pertama Saya!");
+}
+
+function pesanConsole() {
+    console.log("It's Work");
+}
+
+function pesanDok() {
+    document.write("Sucess<br>");
+    document.write("<a href='m4-2.html'>Kembali</a>");
+}
